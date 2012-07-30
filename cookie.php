acc0f1e30d57870e472f71e4c976ffd671f41a55
Miha Rekar <info@mr.si>
2012-07-30 09:44:32 +0200

    return values for setting cookie

--- cookie.php
+++ cookie.php
@@ -1,3 +1,8 @@
 <?
-	setcookie('priority_reservations', true, time() + (60 * 60 * 24 * 7 * 365 * 10), '/');	
+	header('Content-Type: text/html; charset=utf-8');
+	if(setcookie('priority_reservations', true, time() + (60 * 60 * 24 * 7 * 365 * 10), '/')) {
+		echo 'Piškotek nastavljen!';
+	} else {
+		echo 'Piškotka se NE DA nastaviti!!!';
+	}
 ?>
